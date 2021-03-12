6518b7d7315fbba6eed7344fa29cd60495cfabb3
Zvone Gazvoda <zvone.gazvoda@sunesis.si>
2021-03-12 15:47:18 +0100

    Updated style

--- src/components/content/diagram/diagram.js
+++ src/components/content/diagram/diagram.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useTranslation, Link } from "gatsby-plugin-react-i18next"
 import "./diagram.css"
-import image from "../../../../static/images/kumuluz-api-diagram.png"
+import image from "../../../../static/images/kumuluz-api-diagram-large.png"
 
 const DiagramComponent = () => {
 
@@ -9,20 +9,22 @@ const DiagramComponent = () => {
 
   return (
     <div className="diagram">
-      <div className="image-diagram justify-content-md-center">
-        <img src={image} alt="Kumuluz" style={{ width: "100%" }}/>
+      <div className="description-text justify-content-md-center">
+        <span>{t("diagram.description")}</span>
       </div>
       <Link to="/business-details">
         <button className="business-button">
-          <span className="specs-text">{t("diagram.business-button")}</span>
+          <span className="business-text">{t("diagram.business-button")}</span>
         </button>
       </Link>
-
       <Link to="/technical-details">
         <button className="specs-button">
           <span className="specs-text">{t("diagram.specs-button")}</span>
         </button>
       </Link>
+      <div className="image-diagram justify-content-md-center">
+        <img src={image} alt="Kumuluz" style={{ width: "100%" }}/>
+      </div>
     </div>
   )
 }
--- src/components/header/header-content/header-title.js
+++ src/components/header/header-content/header-title.js
@@ -1,8 +1,6 @@
 import React from "react"
 import "./header-content.scss"
-import image from "../../../../static/images/kumuluz-header.png"
 import { useTranslation } from "gatsby-plugin-react-i18next"
-import { CONTACT_URL } from "../../../../static/links"
 
 const HeaderComponent = (props) => {
 
@@ -12,21 +10,6 @@ const HeaderComponent = (props) => {
     <div className="row content-row">
       <div className="col-lg-7">
         <p className="header-title">{t("header." + props.page + "-title")}</p>
-        <p className="header-text">
-          {t("header." + props.page + "-text")}
-        </p>
-        <a href={CONTACT_URL} target="_blank" rel="noreferrer noopener">
-          <button className="contact-button">
-                        <span className="contact-button-text">
-                            {t("header.button-contact")}
-                        </span>
-          </button>
-        </a>
-      </div>
-      <div className="col-lg-5 image-col">
-        <div className="image-container">
-          <img src={image} alt="Kumuluz" className="image"/>
-        </div>
       </div>
     </div>)
 }
