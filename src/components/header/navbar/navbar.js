55aed7db432789f0b3a8afd434873f61b177f24f
Zvone Gazvoda <zvone.gazvoda@sunesis.si>
2021-04-08 12:34:56 +0200

    Updated link to sunesis homepage

--- src/components/header/navbar/navbar.js
+++ src/components/header/navbar/navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import "./navbar.scss"
 import { Link, useTranslation, useI18next } from "gatsby-plugin-react-i18next"
+import {SUNESIS_URL} from "../../../../static/links";
 
 
 const NavbarComponent = () => {
